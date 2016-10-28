5c28cdca83cc410b162c9cd11cdad83b47ba4cb8
Daniel Tomás <ticidici@yahoo.es>
2016-10-28 03:31:09 +0200

    Fix collisions in sewers and implement the two-floors mechanism. Now you can pass through the tunnel and both collisions (physical layer) and sorting layer respond accordingly. For that to work, the stage instance has to be set to "Ground" physical layer, and the upper tile layers have to be set to "Second Floor" sorting layer (Or the desired sorting layer, like "Above Player" for the ceiling).

---
+++ Tiled Test/Assets/Scripts/ChangePhysicalLayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePhysicalLayer : MonoBehaviour {
+
+    public string physicalLayer = "Player";
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer != LayerMask.NameToLayer(physicalLayer))
+        {
+            other.gameObject.layer = LayerMask.NameToLayer(physicalLayer);
+        }
+    }
+}
---
+++ Tiled Test/Assets/Scripts/SwitchFloor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchFloor : MonoBehaviour {
+
+    public string sortingLayer = "Player Second Floor";
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        SpriteRenderer[] spriteRenderers = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
+        if (spriteRenderers == null) { Debug.LogError("Couldn't find any sprite renderer in this object's children", other.gameObject); }
+
+        for (int i = 0; i < spriteRenderers.Length; i++)
+        {
+            string currentSortingLayer = spriteRenderers[i].sortingLayerName;
+            if (spriteRenderers[i].sortingLayerName != sortingLayer)
+            {
+                spriteRenderers[i].sortingLayerName = sortingLayer;
+            }
+        }
+    }
+}
